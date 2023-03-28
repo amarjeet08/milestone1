9339a06c8cd8cbb93f3e0ec1d626e6ca6cbc9386
Amarjeet Singh <71166649+amarjeet08@users.noreply.github.com>
2023-03-28 06:01:58 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+let password = ("Amarjeet Singh");
+let confirmPassword = ("Amarjeet Singh");
+if (password === confirmPassword) {
+  console.log("Password Matched.Password validation Successful.")
+}else{
+  console.log("Password didn't match.Password validation unsuccessful.")
+}
